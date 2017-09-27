f3939f3e669df17395d83e5cea8d25a7c0999f4c
davidperry <dperry.aus@gmail.com>
2017-09-27 15:35:08 +1000

    Get app working... just needs cleaning up now and finish docs.

--- example_client_app/app/app.py
+++ example_client_app/app/app.py
@@ -7,6 +7,15 @@ import datetime
 from config import config, Auth
 from requests_oauthlib import OAuth2Session
 import json
+import tempfile
+import subprocess
+
+from cryptography.hazmat.backends import default_backend
+from cryptography.hazmat.primitives import serialization
+from cryptography.hazmat.primitives.asymmetric import rsa
+from cryptography import x509
+from cryptography.x509.oid import NameOID
+from cryptography.hazmat.primitives import hashes
 
 """
 Simple Flask demo app for accessing HPC over GSISSH using certificates requested from CILogon.
@@ -41,7 +50,14 @@ db = SQLAlchemy(app)
 # Landing Page
 @app.route('/')
 def index():
-    return render_template('app.html')
+
+    cert_subject = ''
+
+    if current_user.is_authenticated:
+        cert = str(current_user.cert)
+        cert_subject = get_certificate_subject(cert)
+
+    return render_template('app.html', cert_subject=cert_subject)
 
 # Login View - simply provides a link directing us to CILogon
 @app.route('/login')
@@ -59,6 +75,22 @@ def login():
 def logout():
     logout_user()
     return redirect(url_for('index'))
+
+
+@app.route('/run', methods=['POST'])
+@login_required
+def run_remote_command():
+    command = request.form.get('command')
+
+    cert_file = create_proxy_certificate_for_user(current_user)
+    command = ['gsissh', '115.146.85.193', '-p', '2222', command]
+
+    try:
+        output = subprocess.check_output(command, env={'X509_USER_PROXY': cert_file}, stderr=subprocess.STDOUT)
+    except subprocess.CalledProcessError as e:
+        output = e.output
+
+    return output
 
 
 # This is where CILogon redirects user once they have been authenticated (or not)
@@ -85,20 +117,132 @@ def callback():
 
         # ... then get user info (email, name, etc.) and create account if necessary.
         cilogon = get_cilogon_auth(token=token)
-        resp = cilogon.get(Auth.USER_INFO)
-        if resp.status_code == 200:
-            user_data = resp.json()
-            email = user_data['email']
-            user = User.query.filter_by(email=email).first()
-            if user is None:
-                user = User()
-                user.email = email
-            user.tokens = json.dumps(token)
-            db.session.add(user)
-            db.session.commit()
-            login_user(user)
-            return redirect(url_for('index'))
-        return 'Could not fetch user information.'
+
+        user = get_user_info(cilogon)
+        get_cert(user, cilogon)
+        login_user(user)
+
+        return redirect(url_for('index'))
+
+
+def get_user_info(cilogon):
+
+    resp = cilogon.get(Auth.USER_INFO)
+
+    print('get_user_info', resp)
+
+    if resp.status_code == 200:
+        user_data = resp.json()
+        email = user_data['email']
+        user = User.query.filter_by(email=email).first()
+
+        # Create user if necessary
+        if user is None:
+            user = User()
+            user.email = email
+
+        user.tokens = json.dumps(cilogon.token)
+        db.session.add(user)
+        db.session.commit()
+
+    else:
+        raise RuntimeError("Error fetching user information.")
+
+    return user
+
+
+def get_cert(user, cilogon):
+    key, csr = get_csr()
+
+    u = cilogon.post(Auth.CERT_URL, params={'client_id':Auth.CLIENT_ID,
+                                            'client_secret': Auth.CLIENT_SECRET,
+                                            'certreq': csr})
+
+    if u.ok:
+        # Save certificate and private key in DB for use later
+        # TODO: Think carefully about best practice here... should we encrypt in situ with another key? Blow away all keys regularly when session old?
+
+        # TODO: Or immediately create proxy cert and save this instead for use during session? Can make that certificate only have say 1hr validity (default is 12).
+        user.key = key
+        user.cert = u.content
+        db.session.add(user)
+        db.session.commit()
+    else:
+        # This can fail because we've already requested a certificate in this session.
+        raise RuntimeError("Error requesting certificate.")
+
+
+
+def get_csr():
+    # Generate private key
+    key = rsa.generate_private_key(
+        public_exponent=65537,
+        key_size=2048,
+        backend=default_backend()
+    )
+
+    # Generate a CSR and sign it. Subject doesn't matter, will be replaced by CILogon service.
+    csr = x509.CertificateSigningRequestBuilder()   \
+        .subject_name(x509.Name([x509.NameAttribute(NameOID.COUNTRY_NAME, u"AU"),]))    \
+        .sign(key, hashes.SHA256(), default_backend())\
+        .public_bytes(serialization.Encoding.PEM)
+
+    # CILogon wants clean request, with no header/footer or newlines.
+    csr = csr.lstrip('-----BEGIN CERTIFICATE REQUEST-----\n').rstrip('-----END CERTIFICATE REQUEST-----\n')
+    csr = csr.replace('\n', '')
+
+    # Return plain text key
+    key = key.private_bytes(encoding=serialization.Encoding.PEM,
+                            format=serialization.PrivateFormat.TraditionalOpenSSL,
+                            encryption_algorithm=serialization.NoEncryption())
+
+    return key, csr
+
+
+def get_certificate_subject(cert):
+    """
+    Parse plain text certificate, extract subject, and format as per that used in GSISSH.
+    """
+    abbreviations = {'domainComponent': 'DC', 'countryName': 'CN', 'organizationName': 'O', 'commonName': 'CN'}
+
+    subject = ''
+
+    for item in x509.load_pem_x509_certificate(cert, default_backend()).subject.rdns:
+        assert (len(item._attributes) == 1)  # Defined as set, but should only be one item
+        attribute = list(item._attributes)[0]
+        value = attribute.value
+        field = attribute.oid._name
+        subject += '/%s=%s' % (abbreviations[field], value)
+
+    return subject
+
+
+def create_proxy_certificate_for_user(user):
+    """
+    Create proxy certificate for user and return its file name.
+    """
+
+    # Write key and certificate to a temp file, will be blown away when file closed.
+    cert_file = tempfile.NamedTemporaryFile()
+    key_file = tempfile.NamedTemporaryFile()
+    cert_file.write(user.cert)
+    key_file.write(user.key)
+    cert_file.flush()  # Force flush so that file contents available to other processes
+    key_file.flush()
+
+    # Resulting proxy cert -- this is retained
+    proxy_cert_file = tempfile.NamedTemporaryFile(delete=False)
+
+    # Create proxy certificate
+    command = ['grid-proxy-init', '-cert', cert_file.name, '-key', key_file.name, '-out', proxy_cert_file.name]
+    subprocess.check_call(command)
+
+    # Clean up
+    cert_file.close()
+    key_file.close()
+    proxy_cert_file.close()
+
+    return proxy_cert_file.name
 
 
 # Hook between our user model and flask_login
@@ -114,6 +258,8 @@ class User(db.Model, UserMixin):
     email = db.Column(db.String(100), unique=True, nullable=False)
     name = db.Column(db.String(100), nullable=True)
     tokens = db.Column(db.Text)
+    cert = db.Column(db.Text)
+    key = db.Column(db.Text)
     created_at = db.Column(db.DateTime, default=datetime.datetime.utcnow())
 
 
